1d566df253ac3345c2a87c4603e6977bd25adc5f
Daniel Sjöblom <daniel@ziney.co>
2022-05-03 12:20:11 +0300

    More work on autoloading

--- lib/alchemy/engine.rb
+++ lib/alchemy/engine.rb
@@ -6,17 +6,23 @@ module Alchemy
     config.mount_at = "/"
 
     initializer "alchemy.lookup_context" do
-      Alchemy::LOOKUP_CONTEXT = ActionView::LookupContext.new(Rails.root.join("app", "views", "alchemy"))
+      config.to_prepare do
+        Alchemy::LOOKUP_CONTEXT = ActionView::LookupContext.new(Rails.root.join("app", "views", "alchemy"))
+      end
     end
 
     initializer "alchemy.dependency_tracker" do
-      [:erb, :slim, :haml].each do |handler|
-        ActionView::DependencyTracker.register_tracker(handler, CacheDigests::TemplateTracker)
+      config.to_prepare do
+        [:erb, :slim, :haml].each do |handler|
+          ActionView::DependencyTracker.register_tracker(handler, CacheDigests::TemplateTracker)
+        end
       end
     end
 
     initializer "alchemy.non_digest_assets" do
-      NonStupidDigestAssets.whitelist += [/^tinymce\//]
+      config.to_prepare do
+        NonStupidDigestAssets.whitelist += [/^tinymce\//]
+      end
     end
 
     # Gutentag downcases all tags before save
@@ -29,19 +35,23 @@ module Alchemy
 
     # Custom Ransack sort arrows
     initializer "alchemy.ransack" do
-      Ransack.configure do |config|
-        config.custom_arrows = {
-          up_arrow: '<i class="fa fas fa-xs fa-arrow-up"></i>',
-          down_arrow: '<i class="fa fas fa-xs fa-arrow-down"></i>',
-        }
+      config.to_prepare do
+        Ransack.configure do |config|
+          config.custom_arrows = {
+            up_arrow: '<i class="fa fas fa-xs fa-arrow-up"></i>',
+            down_arrow: '<i class="fa fas fa-xs fa-arrow-down"></i>',
+          }
+        end
       end
     end
 
     initializer "alchemy.userstamp" do
-      ActiveSupport.on_load(:active_record) do
-        if Alchemy.user_class
-          Alchemy.user_class.model_stamper
-          Alchemy.user_class.stampable(stamper_class_name: Alchemy.user_class.name)
+      config.to_prepare do
+        ActiveSupport.on_load(:active_record) do
+          if Alchemy.user_class
+            Alchemy.user_class.model_stamper
+            Alchemy.user_class.stampable(stamper_class_name: Alchemy.user_class.name)
+          end
         end
       end
     end
